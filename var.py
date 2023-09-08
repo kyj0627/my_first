51abd1168630d97a0c08cb1d530f784f90693097
kyj0627 <audry0627@naver.com>
2023-09-08 17:48:38 +0900

    var.py

---
+++ var.py
@@ -0,0 +1,142 @@
+""" my_var = 10
+print(my_var)
+
+my_list = [1,2,3]
+print(my_list)
+print(*my_list)
+
+my_int = 10
+
+# 부동 소수점
+my_float = 3.14
+
+# 복소수
+my_complex = 3 +4j
+print(my_int)
+
+my_character = 'A'
+my_char = "@"
+print(my_char)
+
+my_string = 'Hello, World!'
+str_name = "python"
+
+my_bool = True
+bFlag = False
+print (my_bool) """
+
+"""  my_list = [1, 2, 'three', True]
+lsElement = [3.14, "b", 'two', False]
+print(*lsElement) """
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+""" print (*my_list)
+print (my_list.__len__())
+print(my_list[3])
+
+list_el = my_list[3]
+print(list_el)
+
+my_list[3] = "t"
+print (*my_list)
+ """
+#print(my_list[3] - my_list[4])
+
+""" print(my_list[2:5])
+slice_ls = my_list[2:5]
+print(slice_ls) """
+
+""" my_list = [10,3,8,9,42,"hello", [2,5, "y"]]
+#my_list.insert(3,"e")
+res = my_list.index(8)
+print(res)
+
+my_list.append(22)
+print(*my_list)
+
+print(my_list.count(8)) """
+
+""" y_list = [10,3,8,9,42,8,"hello"]
+
+print(my_list)
+print(my_list.pop())
+print(*my_list)
+
+print (my_list)
+my_list.sort()
+
+print """
+
+""" my_list = [10,2,8,9,42,8, "hello"]
+list_tmp = [5,7, "world"]
+
+print(my_, list_tmp)
+my_list.extend(list_tmp)
+print(*my_list)
+
+my_tuple = (1, 2, 'three', True)
+tpItem = (3.14, "b", 'two', False)
+my_tup = (4, 2, 6, 1, "py", "w")
+
+print(my_tup.__len__())
+my_tup[2]=9
+print(my_tup) """
+
+""" my_tup = (4, 2, 6, 1, "py", "w", ("h", 8, 7, 3))
+print(my_tup[7])
+
+my_tup = (4,2,6,1,"py", "w")
+print(my_tup.index(2.0.3))
+print(my_tup.inde("py",3,5))
+print(my_tup.inex(1,0,3))
+ """
+
+""" my_dict = {'key1': 'value1', 'key2': 'value2'}
+my_item = my_dict.items()
+print(my_item)
+
+idx = ("key1", "key2", "key3")
+dicts = dict.fromkeys(idx, "0")
+print(dicts)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+dicData = {"name": "python", "number": 23564897}
+
+print(mu_dict["key1"])
+
+my_str = my_dict.get("key2")
+print(my_str)
+
+print(my_dict.pop("key1"))
+print(my_dict) """
+
+""" my_dict = {'key1': 'value1', 'key2': 'value2'}
+dicts = my_dict.copy()
+
+print(dicts)
+print(my_dict) """
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+dicData = {"name": "python", "number": 23564897}
+
+""" my_dict["key3"] = "value3"
+print(my_dict) """
+
+""" my_dict.setdefault("key3")
+print(my_dict)
+
+my_dict.update({"key1":"v4"})
+print(my_dict) """
+
+""" del my_dict["key2"]
+print(my_dict)
+
+print("key2" in my_dict)
+print("key3" in my_dict) """
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+""" my_list = list(my_dict.values())
+print(my_list)
+ """
+my_dict.clear()
+print(my_dict)
